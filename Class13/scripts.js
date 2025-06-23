1a5528821adbbd41ea0d9ecf76fb2562982f971c
Cauã Almeida Moura <hdfser.moura@gmail.com>
2025-06-23 14:40:49 -0300

    Finishing class13

--- Class13/scripts.js
+++ Class13/scripts.js
@@ -1,11 +1,27 @@
 const getCEP = async (cep) => {
-     const CEP = document.getElementById
-    const resultado = await axios.get(`https://cep.awesomeapi.com.br/json/${cep}`);
-    console.dir(resultado.data.address);
-    console.dir(resultado.data.city);
-    console.dir(resultado.data.ddd);
-
-
+    try {
+        const resultado = await axios.get( `https://cep.awesomeapi.com.br/json/${cep}` );
+        const data = resultado.data;
+        const resultDiv = document.getElementById("result");
+        resultDiv.innerHTML = `
+            <strong>Endereço:</strong> ${data.address}<br>
+            <strong>Cidade:</strong> ${data.city}<br>
+            <strong>Local:</strong> ${data.state}<br>
+            <strong>DDD:</strong> ${data.ddd}
+        `;
+    } catch (error) {
+        document.getElementById("result").innerHTML = "CEP não encontrado!";
+    }
 };
 
-getCEP(12301598);
+const cepInput = document.getElementById("cepInput");
+if (cepInput) {
+    cepInput.addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            const cep = event.target.value;
+            if (cep) {
+                getCEP(cep);
+        }
+    }
+    });
+}
